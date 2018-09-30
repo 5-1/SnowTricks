dd8dce4e986979031c342a455a07f8f8dcdf6bed
5-1 <shark51LoL@hotmail.fr>
2018-09-30 18:13:01 +0200

    add BDD and other

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,16 +2,46 @@
 
 namespace AppBundle\Controller;
 
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
 
 class DefaultController extends Controller
 {
     /**
-     * @Route("/")
+     * @Route("/home", name="index")
      */
     public function indexAction()
     {
         return $this->render('default/index.html.twig');
+    }
+
+
+    /**
+     * @Route("/form", name="form")
+     */
+    public function create (Request $request,EntityManagerInterface $manager)
+    {
+
+        $form = $this->createFormBuilder()
+            ->add('title')
+            ->add('content')
+            ->add('image')
+            ->getForm();
+
+        return $this->render('default/log.html.twig', ['form' => $form->createView()]);
+
+
+    }
+
+
+
+    /**
+     * @Route("/tricks", name="tricks")
+     */
+    public  function tricks()
+    {
+        return $this->render('default/tricks.html.twig');
     }
 }
---
+++ src/Appbundle/Entity/trick.php
@@ -0,0 +1,142 @@
+<?php
+
+namespace AppBundle\Entity;
+
+/**
+ * trick
+ */
+class trick
+{
+    /**
+     * @var int
+     */
+    private $id;
+
+    /**
+     * @var string
+     */
+    private $title;
+
+    /**
+     * @var string
+     */
+    private $content;
+
+    /**
+     * @var string
+     */
+    private $image;
+
+    /**
+     * @var \DateTime
+     */
+    private $createdAt;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * Set title
+     *
+     * @param string $title
+     *
+     * @return trick
+     */
+    public function setTitle($title)
+    {
+        $this->title = $title;
+
+        return $this;
+    }
+
+    /**
+     * Get title
+     *
+     * @return string
+     */
+    public function getTitle()
+    {
+        return $this->title;
+    }
+
+    /**
+     * Set content
+     *
+     * @param string $content
+     *
+     * @return trick
+     */
+    public function setContent($content)
+    {
+        $this->content = $content;
+
+        return $this;
+    }
+
+    /**
+     * Get content
+     *
+     * @return string
+     */
+    public function getContent()
+    {
+        return $this->content;
+    }
+
+    /**
+     * Set image
+     *
+     * @param string $image
+     *
+     * @return trick
+     */
+    public function setImage($image)
+    {
+        $this->image = $image;
+
+        return $this;
+    }
+
+    /**
+     * Get image
+     *
+     * @return string
+     */
+    public function getImage()
+    {
+        return $this->image;
+    }
+
+    /**
+     * Set createdAt
+     *
+     * @param \DateTime $createdAt
+     *
+     * @return trick
+     */
+    public function setCreatedAt($createdAt)
+    {
+        $this->createdAt = $createdAt;
+
+        return $this;
+    }
+
+    /**
+     * Get createdAt
+     *
+     * @return \DateTime
+     */
+    public function getCreatedAt()
+    {
+        return $this->createdAt;
+    }
+}
+
