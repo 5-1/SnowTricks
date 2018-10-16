323e5db97ee1a2c1421a844a09e0501abcedcfd7
5-1 <shark51LoL@hotmail.fr>
2018-10-16 14:20:03 +0200

    Un utilisateur peut voir une figure #2

--- src/AppBundle/Controller/DefaultController.php
+++ src/AppBundle/Controller/DefaultController.php
@@ -2,46 +2,89 @@
 
 namespace AppBundle\Controller;
 
+use AppBundle\Entity\Trick;
+use AppBundle\Form\TrickType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 
+/**
+ * Class DefaultController
+ * @package AppBundle\Controller
+ */
 class DefaultController extends Controller
 {
     /**
-     * @Route("/home", name="index")
+     * @Route("/", name="index")
      */
     public function indexAction()
     {
-        return $this->render('default/index.html.twig');
+        $repo = $this->getDoctrine()->getRepository(Trick::class);
+        $trick = $repo->findAll();
+
+        return $this->render('default/index.html.twig', ['controller_name' => 'DefaultController', 'tricks' => $trick]);
     }
 
 
     /**
-     * @Route("/form", name="form")
+     * @Route("/ajouter-une-figure", name="trick_add")
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
      */
-    public function create (Request $request,EntityManagerInterface $manager)
+    public function addTrick(Request $request, EntityManagerInterface $manager)
     {
+        $form = $this->createForm(TrickType::class);
 
-        $form = $this->createFormBuilder()
-            ->add('title')
-            ->add('content')
-            ->add('image')
-            ->getForm();
+        $form->handleRequest($request);
+
+
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $trick = $form->getData();
+            $manager->persist($trick);
+            $manager->flush();
+
+            return $this->redirectToRoute('tricks_show', ['id' => $trick->getId()]);
+        }
 
         return $this->render('default/log.html.twig', ['form' => $form->createView()]);
+    }
+
+    /**
+     * @Route("/editer-une-figure/{id}", name="trick_edit")
+     * @param Trick $trick
+     * @param Request $request
+     * @param EntityManagerInterface $manager
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     */
+    public function editTrick(Trick $trick, Request $request, EntityManagerInterface $manager)
+    {
+        $form = $this->createForm(TrickType::class, $trick);
+
+        $form->handleRequest($request);
 
 
+        if ($form->isSubmitted() && $form->isValid()) {
+
+            $trick = $form->getData();
+            $manager->flush();
+
+            return $this->redirectToRoute('tricks_show', ['id' => $trick->getId()]);
+        }
+
+        return $this->render('default/log.html.twig', ['form' => $form->createView()]);
     }
 
 
-
     /**
-     * @Route("/tricks", name="tricks")
+     * @Route("/trick/{id}", name="tricks_show")
+     * @param Trick $trick
+     * @return \Symfony\Component\HttpFoundation\Response
      */
-    public  function tricks()
+    public function trickShow(Trick $trick)
     {
-        return $this->render('default/tricks.html.twig');
+        return $this->render('default/tricks.html.twig', ['trick' => $trick]);
     }
 }
