b314ddcc98db1781738e585a94e983b422d7bf63
5-1 <shark51LoL@hotmail.fr>
2018-11-02 15:19:10 +0100

    Catagory : create entity and repository #8

--- src/AppBundle/DataFixtures/ORM/TrickFixtures.php
+++ src/AppBundle/DataFixtures/ORM/TrickFixtures.php
@@ -2,20 +2,36 @@
 
 namespace AppBundle\DataFixtures\ORM;
 
+use AppBundle\Entity\Category;
 use AppBundle\Entity\Trick;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
 class TrickFixtures extends Fixture
 {
+    /**
+     * @param ObjectManager $manager
+     * @throws \Doctrine\Common\DataFixtures\BadMethodCallException
+     */
     public function load(ObjectManager $manager)
     {
+        $categories = ['Grabs', 'Rotations'];
+
+        foreach ($categories as $cat){
+            $category = new Category();
+            $category->setName($cat);
+            $manager->persist($category);
+            $this->addReference('CAT_'.$cat, $category);
+        }
+
+
         for ($i = 1; $i <= 10; $i++) {
             $trick = new Trick();
             $trick->setTitle("Titre du tricks n$i")
                 ->setContent("<p>contenu tricks n$i</p>")
                 ->setImage("http://placehold.it/350*150")
                 ->setCreatedAt(new \DateTime());
+            $trick->setCategory($this->getReference('CAT_Grabs'));
             $manager->persist($trick);
         }
         $manager->flush();
---
+++ src/AppBundle/Entity/Category.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace AppBundle\Entity;
+use Doctrine\ORM\Mapping as ORM;
+use Gedmo\Mapping\Annotation as Gedmo;
+
+
+/**
+ * Category
+ * @ORM\Table(name="categories")
+ * @ORM\Entity(repositoryClass="AppBundle\Repository\CategoryRepository")
+ */
+class Category
+{
+    /**
+     * @var int
+     *
+     * @ORM\Column(name="id", type="integer")
+     * @ORM\Id()
+     * @ORM\GeneratedValue(strategy="AUTO")
+     */
+    private $id;
+
+    /**
+     * @var string
+     * @ORM\Column(type="string", length=255, nullable=false)
+     */
+    private $name;
+
+
+    /**
+     * Get id
+     *
+     * @return int
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+
+    /**
+     * Set name
+     *
+     * @param string $name
+     *
+     * @return Category
+     */
+    public function setName($name)
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    /**
+     * Get name
+     *
+     * @return string
+     */
+    public function getName()
+    {
+        return $this->name;
+    }
+
+    public function __toString()
+    {
+        return $this->name;
+    }
+
+
+}
