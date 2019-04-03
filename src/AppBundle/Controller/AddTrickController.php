bdc16f0aa287e60ce3dc968a1ff7e4aac56d68c2
5-1 <shark51LoL@hotmail.fr>
2019-04-03 02:04:55 +0200

    add is granted for add tricks

--- src/AppBundle/Controller/AddTrickController.php
+++ src/AppBundle/Controller/AddTrickController.php
@@ -7,9 +7,11 @@ use AppBundle\Entity\Trick;
 use AppBundle\Form\TrickType;
 use AppBundle\Service\FileUploader;
 use Doctrine\ORM\EntityManagerInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\IsGranted;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
+
 
 class AddTrickController extends Controller
 {
@@ -18,6 +20,7 @@ class AddTrickController extends Controller
     /**
      * @Route("/ajouter-une-figure", name="trick_add")
      * @param Request $request
+     * @IsGranted("ROLE_USER")
      * @param EntityManagerInterface $manager
      * @param FileUploader $fileUploader
      * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
